f102765ed09786d761d25daab833ec80a15d70b7
FocusRiabilitazione <riabilitazionescandiano@gmail.com>
2026-02-10 11:07:08 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -8,4 +8,9 @@ if db_url.startswith("postgres://"):
 engine = create_engine(db_url, echo=False)
 
 def init_db():
+    # --- COMANDO DI RESET ---
+    # Cancella tutto il vecchio database (sblocca l'errore)
+    SQLModel.metadata.drop_all(engine)
+    
+    # Crea le nuove tabelle pulite
     SQLModel.metadata.create_all(engine)
